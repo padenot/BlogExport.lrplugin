c5ad21a83a4157b8d4d20ebb501412aa15cf58f4
Paul Adenot <paul@paul.cx>
2017-07-09 13:15:56 +0200

    don't force setting a date and text and all.

--- BlogAPI.lua
+++ BlogAPI.lua
@@ -19,19 +19,6 @@ function BlogAPI.uploadWork(metadata, photoFilePath)
       mimeChunks[#mimeChunks + 1] = { name = k, value = v}
     end
 
-    -- sane default values in case the user hasn't entered anything
-    if metadata["title"] == nil or metadata["title"] == "" then
-      mimeChunks[#mimeChunks + 1] = { name = "title", value = "."}
-    end
-
-    if metadata["text"] == nil or metadata["text"] == "" then
-      mimeChunks[#mimeChunks + 1] = { name = "text", value = "."}
-    end
-
-    if metadata["publish_date"] == nil or metadata["publish_date"] == "" then
-      mimeChunks[#mimeChunks + 1] = { name = "publish_date", value = LrDate.timeToUserFormat(LrDate.currentTime(), "%Y-%m-%d", false) }
-    end
-
     local fileName = LrPathUtils.leafName(photoFilePath)
     mimeChunks[#mimeChunks + 1] = { name = 'photo', fileName = fileName, filePath = photoFilePath, contentType = 'image/jpeg' }
     mimeChunks[#mimeChunks + 1] = { name = 'passwd', value = 'plop'}
